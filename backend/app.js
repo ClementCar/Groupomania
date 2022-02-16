eeafe4c87e6d5d6a426444ea4e946e04a299cdcf
Clement Carcenac <clementcarcenac@gmail.com>
2022-02-16 19:27:57 +0100

    database sql

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const { Sequelize } = require('sequelize');
 
 
+app.use(express.json())
 
 // Création des en-têtes
 app.use((req, res, next) => {
@@ -10,5 +12,21 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+const sequelize = new Sequelize('Groupomania', 'root', 'SQLmotDEpasse20', {
+    dialect: 'mysql',
+    host: 'localhost'
+});
+
+try {
+    sequelize.authenticate();
+    console.log('Connecté à la base de données MySQL !');
+    // sequelize.query('CREATE DATABASE `Groupomania`;')
+    //     .then(([results, metadata]) => {
+    //         console.log('Base de données crée !');
+    //     })
+} catch (error) {
+    console.error('Impossible de se connecter, erreur suivante :', error);
+}
 
 module.exports = app;
