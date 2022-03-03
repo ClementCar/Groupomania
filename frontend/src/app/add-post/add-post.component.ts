090502fdce58b288ed2796513017451d69fd36ab
Clement Carcenac <clementcarcenac@gmail.com>
2022-03-03 20:05:48 +0100

    add post

--- frontend/src/app/add-post/add-post.component.ts
+++ frontend/src/app/add-post/add-post.component.ts
@@ -1,4 +1,8 @@
+import { convertPropertyBinding } from '@angular/compiler/src/compiler_util/expression_converter';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { cpuUsage } from 'process';
+import { PostServices } from 'src/services/post.services';
 
 @Component({
   selector: 'app-add-post',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private postService: PostServices
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  addPost(): void {
+    const title = (<HTMLInputElement>document.getElementById('title')).value;
+    const content = (<HTMLInputElement>document.getElementById('content')).value;
+    const attachment = (<HTMLInputElement>document.getElementById('attachment')).value;
+    this.postService.addPost(title, content, attachment).subscribe({
+      next: data => console.log(data),
+      error: error => console.log(error),
+      complete: () => this.router.navigateByUrl('/')
+    })
   }
 
 }
